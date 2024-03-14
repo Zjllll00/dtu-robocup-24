03c7ed4cccefea0eb41b479277373b4d27cc23e4
G Côte <meltwin158@gmail.com>
2024-03-14 23:26:11 +0100

    Improved yolo processing unit

--- dtu_robocup_24/image/yolo_processor.py
+++ dtu_robocup_24/image/yolo_processor.py
@@ -1,16 +1,21 @@
-import cv2 as cv
 import os
-from raubase_ros.interface import CVImage, ImageProcessingUnit
-from rclpy.logging import get_logger
-from ultralytics import YOLO
+from typing import Dict, Iterator, List, Tuple
+
+import cv2 as cv
 from ament_index_python import get_package_share_directory
-from raubase_ros.wrappers import NodeWrapper
 from raubase_msgs.msg import ResultYolo, YoloObject
+from raubase_ros.interface import CVImage, ImageProcessingUnit
+from raubase_ros.utils import DimType
+from raubase_ros.wrappers import NodeWrapper, ParameterWrapper
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
+
+YOLOResults = List[Results]
 
 
 class MLObjectProcessor(ImageProcessingUnit):
     """
-    Detect golf ball with trained AI
+    Detect objects with trained AI (using the YOLO library).
     """
 
     # ROS things
@@ -24,10 +29,12 @@ class MLObjectProcessor(ImageProcessingUnit):
     TEXT_COLOR = (255, 255, 255)
     TEXT_THICK = 2
 
-    def __init__(self):
-        super().__init__()
+    # =================================================================
+    #                          Initializations
+    # =================================================================
 
-        self.logger = get_logger("YOLOProcessor")
+    def __init__(self):
+        super().__init__("YOLOProcessor")
 
     def load_model(self, model_name: str) -> YOLO | None:
         # Get model file
@@ -35,10 +42,10 @@ class MLObjectProcessor(ImageProcessingUnit):
         file_path = os.path.join(share_dir, "model", f"{model_name}.pt")
 
         if os.path.exists(file_path):
-            self.logger.info(f"Loading model {model_name}")
+            self._logger.info(f"Loading model {model_name}")
             return YOLO(file_path)
         else:
-            self.logger.error(f"Model {model_name} not found !")
+            self._logger.error(f"Model {model_name} not found !")
             return None
 
     def setup(self, node: NodeWrapper) -> None:
@@ -46,16 +53,93 @@ class MLObjectProcessor(ImageProcessingUnit):
         model_name = node.declare_wparameter("yolo_model", "ball_detection").get()
         self.model: YOLO | None = self.load_model(model_name)
 
-        # If model loaded, set an output topic
-        if self.model is not None:
-            self.yolo_msg = ResultYolo()
-            self.yolo_msg.detected = []
-            self.yolo_pub = node.create_publisher(
-                ResultYolo,
-                MLObjectProcessor.YOLO_OUTPUT_TOPIC,
-                10,
-            )
+        if self.model is None:
+            return
 
+        # If model loaded, set an output topic
+        self.yolo_msg = ResultYolo()
+        self.yolo_msg.detected = []
+        self.yolo_pub = node.create_publisher(
+            ResultYolo,
+            MLObjectProcessor.YOLO_OUTPUT_TOPIC,
+            10,
+        )
+
+        # Load classes' width
+        self.widths: Dict[str, ParameterWrapper] = {
+            "ball": node.declare_wparameter("ball_width", 0.04),
+            "house": node.declare_wparameter("house_width", 0.1),
+            "trolley": node.declare_wparameter("trolley_width", 0.06),
+        }
+        self.min_conf = node.declare_wparameter("min_conf", 0.6)
+
+    # =================================================================
+    #                           Methods
+    # =================================================================
+    @staticmethod
+    def box_results(
+        results: YOLOResults,
+    ) -> Iterator[Tuple[Tuple[float, float, float, float], float, str]]:
+        """
+        Iterate over the results of the model
+        """
+        for r in results:
+            for box, conf, c_id in zip(r.boxes.xyxy, r.boxes.conf, r.boxes.cls):
+                yield (box, float(conf), r.names[c_id])
+
+    def make_yolo_obj(
+        self, boxes: Tuple[float, float, float, float], conf: float, class_name: str
+    ) -> YoloObject:
+        """
+        Construct the YOLO object based on the given box.
+        """
+        r = YoloObject()
+
+        # Make image position
+        r.xmin, r.ymin, r.xmax, r.ymax = map(int, boxes)
+        r.confidence = conf
+        r.classifier = class_name
+
+        if r.classifier in self.widths.keys():
+            real_width = self.widths.get(r.classifier).get()
+        else:
+            real_width = 0.1
+
+        # Compute coordinates in robot frame
+        width, height = (r.xmax - r.xmin), (r.ymax - r.ymin)
+        xc, yc = r.xmin + width / 2, r.ymin + height / 2
+        r.robot_x, r.robot_y, r.robot_z = self.in_robot_frame(
+            xc, yc, width, real_width, dim_type=DimType.WIDTH
+        )
+        return r
+
+    def draw_debug(self, debug_img: CVImage, obj: YoloObject) -> None:
+        """
+        Draw the object onto the debug image.
+        """
+        # Draw the bounding box on the frame
+        cv.rectangle(
+            debug_img,
+            (obj.xmin, obj.ymin),
+            (obj.xmax, obj.ymax),
+            MLObjectProcessor.RECT_COLOR,
+            MLObjectProcessor.RECT_WIDTH,
+        )
+
+        # Get and draw the class name and confidence
+        cv.putText(
+            debug_img,
+            f"{obj.classifier} {obj.confidence:.2f}",
+            (obj.xmin, obj.ymin - 10),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            MLObjectProcessor.TEXT_COLOR,
+            MLObjectProcessor.TEXT_THICK,
+        )
+
+    # =================================================================
+    #                           Loop
+    # =================================================================
     def run(
         self,
         img: CVImage,
@@ -70,52 +154,17 @@ class MLObjectProcessor(ImageProcessingUnit):
 
         # Else compute results from model
         rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        results = self.model([rgb_img], stream=True)
+        results: YOLOResults = self.model([rgb_img], stream=True)
 
         if results is None:
             return
 
-        # Exploit results
-        for result in results:
-            # Access the bounding box coordinates directly using the xyxy property
-            # Assuming result.boxes is accessible and xyxy gives us the bounding box coordinates
-            # Accessing confidence scores for each bounding box
-            # Accessing class IDs for each bounding box
-            [boxes, confs, classes] = [
-                result.boxes.xyxy,
-                result.boxes.conf,
-                result.boxes.cls,
-            ]
+        for box, conf, class_name in MLObjectProcessor.box_results(results):
+            if conf > self.min_conf.get():
+                obj = self.make_yolo_obj(box, float(conf), class_name)
+                self.yolo_msg.detected.append(obj)
 
-            # Iterate through each bounding box
-            for box, conf, cls_id in zip(boxes, confs, classes):
-                if conf > 0.6:  # Filter boxes with confidence > 0.6
+                if print_debug:
+                    self.draw_debug(debug_img, obj)
 
-                    r = YoloObject()
-                    r.xmin, r.ymin, r.xmax, r.ymax = map(int, box)
-                    r.confidence = float(conf)
-                    r.classifier = result.names[int(cls_id)]
-
-                    self.yolo_msg.detected.append(r)
-
-                    if print_debug:
-                        # Draw the bounding box on the frame
-                        cv.rectangle(
-                            debug_img,
-                            (r.xmin, r.ymin),
-                            (r.xmax, r.ymax),
-                            MLObjectProcessor.RECT_COLOR,
-                            MLObjectProcessor.RECT_WIDTH,
-                        )
-
-                        # Get and draw the class name and confidence
-                        cv.putText(
-                            debug_img,
-                            f"{r.classifier} {r.confidence:.2f}",
-                            (r.xmin, r.ymin - 10),
-                            cv.FONT_HERSHEY_SIMPLEX,
-                            0.5,
-                            MLObjectProcessor.TEXT_COLOR,
-                            MLObjectProcessor.TEXT_THICK,
-                        )
         self.yolo_pub.publish(self.yolo_msg)
